48f8a87e915647f9c62a51ec31c86e91f83019af
vanhauen <proxy.mbvh@protonmail.com>
2016-06-16 12:41:49 +0200

    Calculator-0.0.csproj

---
+++ Controller.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using CalculatorModules; 
+
+
+namespace Calculator0
+{
+	class MainClass
+	{  
+
+		public static void Main (string[] args)
+		{
+			//Deklaration af variabler
+			double num1 = 0; 
+			double num2 = 0;
+			double answer = 0;
+			string operand;
+			bool continue2 = true; 
+
+			//Deklaration af klasse [Valg af regneoperation]
+			UdvidedeBeregningerClass udregn = new UdvidedeBeregningerClass ();
+
+			// Få input fra bruger. Do while løkken sikre at bruger kan regne videre på sidste resultat samt 
+			//Afslutning af programmet ved at indtaste "q"
+
+				Console.WriteLine ("Indtast dit første tal");
+				num1 = Convert.ToDouble(Console.ReadLine ());
+
+			do 
+				{
+				
+				Console.WriteLine ("Indtast din operator [+, -, *, /, ^]");
+				operand = Console.ReadLine();
+
+				if(operand == "q")
+					{
+						continue2 = false; 
+					} 
+				else 
+					{
+						Console.WriteLine ("Indtast dit andet tal");
+						num2 = Convert.ToDouble(Console.ReadLine ());
+						
+						// Kald af klasse som kalder den klasse der indeholde den pågældende regneoperation
+						answer = udregn.Beregner2(num1, num2, operand); 
+						
+						//Print af resulatat og videreførsel af resultat til næste cyklus.
+						Console.WriteLine(answer); 
+						num1 = answer; 
+					}
+				}
+			while (continue2);
+		}
+	}
+}
---
+++ Modules.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace CalculatorModules
+{
+	class BeregnerClass 
+	{
+		// Foretager simple regneoperationer [+,-,*,/]
+		public double Beregner (double num1, double num2, string operand) 
+		{ 
+			double answer = 0; 
+
+			switch (operand) 
+			{
+				case "+":
+					answer = num1 + num2;
+					break; 
+
+				case "-":
+					answer = num1 - num2;
+					break;
+
+				case "*":
+					answer = num1 * num2;
+					break;
+
+				case "/":
+					answer = num1 / num2; 
+					break;
+
+					default: 
+					answer = 0; 
+					break; 
+			}
+			return answer; 
+		}
+
+	}
+
+	class UdvidedeBeregningerClass : BeregnerClass
+	{ 
+		// Udvidelse af de regneoperationer der kan foretages af Beregner metoden
+		public double Beregner2 (double num1, double num2, string operand) 
+		{  
+			double answer = 0; 
+
+			if (operand == "+" || operand == "-" || operand == "*" || operand == "/") 
+			{ 
+				answer = Beregner (num1, num2, operand); 
+				return answer; 
+			} 
+			else 
+			{
+				switch (operand) 
+				{
+				case "^": 
+					answer = Math.Pow (num1, num2); 
+						break;
+
+					default: 
+						answer = 0; 
+						break; 
+				} 
+				return answer; 
+			}
+		} 
+	}
+}
+	
